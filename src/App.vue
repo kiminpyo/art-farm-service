d3cc2f5d4e3abed849fb7b686ab3b8ba4f316e5a
kiminpyo <rladlsvy2448@naver.com>
2022-01-13 15:41:45 +0900

    2022 0113 341

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,7 @@
     >
       <template v-slot:img="{ props }">
         <v-img
-      
-          v-bind="props"
+         v-bind="props"
           gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
         ></v-img>
       </template>
